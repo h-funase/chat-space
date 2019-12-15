17d1976a779c070db287173d8ea213f47f3390c5
h-funase <h.funse@gmail.com>
2019-12-15 14:13:00 +0900

    re_re

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -89,7 +89,7 @@ $(function(){
       .done(function (messages) { 
         var insertHTML = '';
         $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
+          insertHTML = buildHTML(message)
           $('.messages').append(insertHTML);
          })
           $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
